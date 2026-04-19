c7371b438431dba9a36f54475812fb4ca9082d3c
Gelson Simoes <giovanna@verticalparts.com.br>
2026-04-18 21:22:31 -0300

    fix: corrigir estrutura de arquivos Cypress e porta 8080 no workflow

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+﻿const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:8080',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
+    video: true,
+    screenshotOnRunFailure: true,
+  },
+});
