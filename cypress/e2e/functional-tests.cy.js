cc25f10f6e7159f839308c4a259a736a98988565
Gelson Simoes <giovanna@verticalparts.com.br>
2026-04-18 21:53:26 -0300

    feat: adicionar testes funcionais específicos para VP Click

---
+++ cypress/e2e/functional-tests.cy.js
@@ -0,0 +1,32 @@
+﻿describe('Testes Funcionais - VP Click', () => {
+  
+  beforeEach(() => {
+    cy.visit('/')
+    Cypress.config('defaultCommandTimeout', 10000)
+  })
+
+  it('Verifica se a página inicial carrega corretamente', () => {
+    cy.url().should('include', 'localhost')
+    cy.get('body').should('be.visible')
+    cy.title().should('not.be.empty')
+  })
+
+  it('Verifica se existem botões interativos na página', () => {
+    cy.get('body').find('button').should('have.length.gte', 0)
+  })
+
+  it('Verifica se existem links de navegação', () => {
+    cy.get('body').find('a[href]').should('have.length.gte', 0)
+  })
+
+  it('Testa responsividade básica - viewport mobile', () => {
+    cy.viewport('iphone-6')
+    cy.get('body').should('be.visible')
+  })
+
+  it('Testa responsividade básica - viewport desktop', () => {
+    cy.viewport(1920, 1080)
+    cy.get('body').should('be.visible')
+  })
+
+})
